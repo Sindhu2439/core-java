eb6a3a22e522c77bee74caa7f859edd1f2383fe7
Sindhu2439 <sindhumvsindhugowda@gmail.com>
2026-04-11 22:15:31 +0530

    April 11th practiced programmes

---
+++ Array/Demo.java
@@ -0,0 +1,18 @@
+class Demo{
+     public static void main(String... args){
+	   int[][] array = new int[3][4];
+	   for(int i=0; i<3;i++){
+	       for(int j=0; j<4;j++){
+		       array[i][j]=(int)(Math.random()*100);
+		       System.out.print(array[i][j] + " ");
+		   }
+	          System.out.println();
+	   }
+	   
+	 
+	 
+	 
+	 }
+
+
+}
