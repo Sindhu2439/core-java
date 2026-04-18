3152ffee0d92b568570814b22c0c53b29123f560
Sindhu2439 <sindhumvsindhugowda@gmail.com>
2026-04-18 20:58:51 +0530

    April 18th practiced questionss

---
+++ Array/Address.java
@@ -0,0 +1,11 @@
+class Address {
+    int no;
+    int pincode;
+    State state;
+
+    Address(int no, int pincode, State state) {
+        this.no = no;
+        this.pincode = pincode;
+        this.state = state;
+    }
+}
