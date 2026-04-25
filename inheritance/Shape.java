f0d92c1323d0f7393ff622402436618dc3e94ca0
Sindhu2439 <sindhumvsindhugowda@gmail.com>
2026-04-25 22:19:39 +0530

    April 25th practiced programs

---
+++ inheritance/Shape.java
@@ -0,0 +1,28 @@
+class Shape {
+    void draw() {
+        System.out.println("Drawing shape");
+    }
+}
+
+class Circle extends Shape {
+    void draw() {
+        System.out.println("Drawing circle");
+    }
+}
+
+class Rectangle extends Shape {
+    void draw() {
+        System.out.println("Drawing rectangle");
+    }
+}
+
+class ShapeRunner {
+    public static void main(String[] args) {
+
+        Shape shape1 = new Circle();
+        Shape shape2 = new Rectangle();
+
+        shape1.draw();
+        shape2.draw();
+    }
+}
