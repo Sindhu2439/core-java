021ed7b06aa882ab6872d69dca20913ee6f3cde0
Sindhu2439 <sindhumvsindhugowda@gmail.com>
2026-05-04 09:50:09 +0530

    May 4th practiced questions

---
+++ inheritance/Traingle.java
@@ -0,0 +1,22 @@
+class Traingle{
+        public static void main(String[] args) {
+		
+		 n=5;
+      for(int i=1; i <= n;i++){
+	      for(int j=1; j<=n-1; j++){
+		  
+		     System.out.print("");
+		  
+		  }
+		  for(k=1;k<=i:k++){
+	      System.out.print("*");
+	  }
+	  }
+	  
+    System.out.println();
+ }
+ }
+ 
+
+
+
