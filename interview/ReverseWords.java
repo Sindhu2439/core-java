afb9862bbc48e618f604f3a5ddd3771d96a3816e
Sindhu2439 <sindhumvsindhugowda@gmail.com>
2026-05-07 20:12:11 +0530

    may 7th practiced question

---
+++ interview/ReverseWords.java
@@ -0,0 +1,13 @@
+class ReverseWords {
+    public static void main(String[] args) {
+        String str = "Java is powerful";
+        String[] words = str.split(" ");
+        String result = "";
+
+        for (int i = words.length - 1; i >= 0; i--) {
+            result += words[i] + " ";
+        }
+
+        System.out.println(result.trim());
+    }
+}
